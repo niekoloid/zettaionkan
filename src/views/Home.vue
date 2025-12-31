f1dc09c6ac14a8e31e1059268c0176b4e5a7b1e0
Shun Shiku <sagong.sun@gmail.com>
2026-01-01 01:30:07 +0900

    Fix Steinway B audio and update score display size to 250x500

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -45,6 +45,7 @@ const selectedPiano = ref('yamaha') // 'yamaha' or 'steinway'
 let yamahaSampler = null
 let steinwaySampler = null
 
+// Yamaha C5 (Salamander) mapping
 const YAMAHA_C5_SAMPLES = {
   "A0": "A0.mp3", "C1": "C1.mp3", "D#1": "Ds1.mp3", "F#1": "Fs1.mp3",
   "A1": "A1.mp3", "C2": "C2.mp3", "D#2": "Ds2.mp3", "F#2": "Fs2.mp3",
@@ -56,16 +57,16 @@ const YAMAHA_C5_SAMPLES = {
   "A7": "A7.mp3", "C8": "C8.mp3"
 }
 
-// Steinway Model B samples (using standard Tone.js compatible mapping)
+// Steinway B (nbrosowsky) mapping - using standard note names as expected by that repo
 const STEINWAY_B_SAMPLES = {
-  "A0": "A0.mp3", "C1": "C1.mp3", "D#1": "Ds1.mp3", "F#1": "Fs1.mp3",
-  "A1": "A1.mp3", "C2": "C2.mp3", "D#2": "Ds2.mp3", "F#2": "Fs2.mp3",
-  "A2": "A2.mp3", "C3": "C3.mp3", "D#3": "Ds3.mp3", "F#3": "Fs3.mp3",
-  "A3": "A3.mp3", "C4": "C4.mp3", "D#4": "Ds4.mp3", "F#4": "Fs4.mp3",
-  "A4": "A4.mp3", "C5": "C5.mp3", "D#5": "Ds5.mp3", "F#5": "Fs5.mp3",
-  "A5": "A5.mp3", "C6": "C6.mp3", "D#6": "Ds6.mp3", "F#6": "Fs6.mp3",
-  "A6": "A6.mp3", "C7": "C7.mp3", "D#7": "Ds7.mp3", "F#7": "Fs7.mp3",
-  "A7": "A7.mp3", "C8": "C8.mp3"
+  "A0": "A0.mp3", "C1": "C1.mp3", "C#1": "Cs1.mp3", "D1": "D1.mp3", "D#1": "Ds1.mp3", "E1": "E1.mp3", "F1": "F1.mp3", "F#1": "Fs1.mp3", "G1": "G1.mp3", "G#1": "Gs1.mp3", "A1": "A1.mp3", "A#1": "As1.mp3", "B1": "B1.mp3",
+  "C2": "C2.mp3", "C#2": "Cs2.mp3", "D2": "D2.mp3", "D#2": "Ds2.mp3", "E2": "E2.mp3", "F2": "F2.mp3", "F#2": "Fs2.mp3", "G2": "G2.mp3", "G#2": "Gs2.mp3", "A2": "A2.mp3", "A#2": "As2.mp3", "B2": "B2.mp3",
+  "C3": "C3.mp3", "C#3": "Cs3.mp3", "D3": "D3.mp3", "D#3": "Ds3.mp3", "E3": "E3.mp3", "F3": "F3.mp3", "F#3": "Fs3.mp3", "G3": "G3.mp3", "G#3": "Gs3.mp3", "A3": "A3.mp3", "A#3": "As3.mp3", "B3": "B3.mp3",
+  "C4": "C4.mp3", "C#4": "Cs4.mp3", "D4": "D4.mp3", "D#4": "Ds4.mp3", "E4": "E4.mp3", "F4": "F4.mp3", "F#4": "Fs4.mp3", "G4": "G4.mp3", "G#4": "Gs4.mp3", "A4": "A4.mp3", "A#4": "As4.mp3", "B4": "B4.mp3",
+  "C5": "C5.mp3", "C#5": "Cs5.mp3", "D5": "D5.mp3", "D#5": "Ds5.mp3", "E5": "E5.mp3", "F5": "F5.mp3", "F#5": "Fs5.mp3", "G5": "G5.mp3", "G#5": "Gs5.mp3", "A5": "A5.mp3", "A#5": "As5.mp3", "B5": "B5.mp3",
+  "C6": "C6.mp3", "C#6": "Cs6.mp3", "D6": "D6.mp3", "D#6": "Ds6.mp3", "E6": "E6.mp3", "F6": "F6.mp3", "F#6": "Fs6.mp3", "G6": "G6.mp3", "G#6": "Gs6.mp3", "A6": "A6.mp3", "A#6": "As6.mp3", "B6": "B6.mp3",
+  "C7": "C7.mp3", "C#7": "Cs7.mp3", "D7": "D7.mp3", "D#7": "Ds7.mp3", "E7": "E7.mp3", "F7": "F7.mp3", "F#7": "Fs7.mp3", "G7": "G7.mp3", "G#7": "Gs7.mp3", "A7": "A7.mp3", "A#7": "As7.mp3", "B7": "B7.mp3",
+  "C8": "C8.mp3"
 }
 
 onMounted(() => {
@@ -77,11 +78,15 @@ onMounted(() => {
     }
   }).toDestination()
 
+  // Use a more robust CDN source for Steinway MP3s
   steinwaySampler = new Tone.Sampler({
     urls: STEINWAY_B_SAMPLES,
     baseUrl: "https://nbrosowsky.github.io/tonejs-instruments/samples/piano/",
     onload: () => {
       if (selectedPiano.value === 'steinway') isSamplerLoaded.value = true
+    },
+    onerror: (err) => {
+      console.error("Steinway loading error:", err)
     }
   }).toDestination()
 })
@@ -89,12 +94,12 @@ onMounted(() => {
 const renderScore = (abc) => {
   abcjs.renderAbc('chord-score', `L:1/4\nK:C\n${abc}`, {
     responsive: 'resize',
-    scale: 1.8,
+    scale: 2.2, // Increased scale for larger display
     paddingtop: 0,
     paddingbottom: 0,
     paddingleft: 0,
     paddingright: 0,
-    staffwidth: 120,
+    staffwidth: 140, // Increased staffwidth
     add_classes: true
   })
 }
@@ -179,9 +184,9 @@ const getBlackKeyNote = (whiteNote) => {
       <section class="flex flex-col items-center space-y-6 mb-10">
         <div class="flex flex-col items-center">
           <h2 class="text-[10px] font-bold text-gray-300 uppercase tracking-widest mb-2">譜面表示</h2>
-          <div class="w-full max-w-[200px] max-h-[200px] aspect-square bg-gray-50 rounded-3xl p-2 flex flex-col items-center justify-center border border-gray-100 shadow-inner overflow-hidden">
+          <div class="w-full max-w-[250px] max-h-[500px] bg-gray-50 rounded-3xl p-4 flex flex-col items-center justify-center border border-gray-100 shadow-inner overflow-hidden">
             <div v-show="currentChord" id="chord-score" class="w-full flex justify-center items-center"></div>
-            <p v-if="!currentChord" class="text-gray-300 text-xs italic font-medium">タップ</p>
+            <p v-if="!currentChord" class="text-gray-300 text-sm italic font-medium">タップ</p>
           </div>
         </div>
 
