4b54cfd417b4c8ad5d71b2bddfa7baa1d8323bfa
Shun Shiku <sagong.sun@gmail.com>
2026-01-01 22:08:17 +0900

    Change all color names specifically to hiragana

--- src/constants/chords.js
+++ src/constants/chords.js
@@ -4,7 +4,7 @@ export const ChordDefinitions = {
     id: 'domiso', 
     name: 'ドミソ', 
     symbol: 'C', 
-    colorName: '赤', 
+    colorName: 'あか', 
     color: '#EF4444', 
     notes: ['C4', 'E4', 'G4'], 
     abc: '[CEG]' 
@@ -13,7 +13,7 @@ export const ChordDefinitions = {
     id: 'dofara', 
     name: 'ドファラ', 
     symbol: 'F/C', 
-    colorName: '黄', 
+    colorName: 'きいろ', 
     color: '#FFFF00', 
     notes: ['C4', 'F4', 'A4'], 
     abc: '[CFA]' 
@@ -22,7 +22,7 @@ export const ChordDefinitions = {
     id: 'shireso', 
     name: 'シレソ', 
     symbol: 'G/B', 
-    colorName: '青', 
+    colorName: 'あお', 
     color: '#3B82F6', 
     notes: ['B3', 'D4', 'G4'], 
     abc: '[B,DG]' 
@@ -33,7 +33,7 @@ export const ChordDefinitions = {
     id: 'resoshi', 
     name: 'レソシ', 
     symbol: 'G/D', 
-    colorName: '緑', 
+    colorName: 'みどり', 
     color: '#22C55E', 
     notes: ['D4', 'G4', 'B4'], 
     abc: '[DGB]' 
@@ -42,7 +42,7 @@ export const ChordDefinitions = {
     id: 'misodo', 
     name: 'ミソド', 
     symbol: 'C/E', 
-    colorName: 'オレンジ', 
+    colorName: 'おれんじ', 
     color: '#F97316', 
     notes: ['E4', 'G4', 'C5'], 
     abc: '[EGc]' 
@@ -51,7 +51,7 @@ export const ChordDefinitions = {
     id: 'radofa', 
     name: 'ラドファ', 
     symbol: 'F/A', 
-    colorName: '紫', 
+    colorName: 'むらさき', 
     color: '#A855F7', 
     notes: ['A3', 'C4', 'F4'], 
     abc: '[A,CF]' 
@@ -60,7 +60,7 @@ export const ChordDefinitions = {
     id: 'soshire', 
     name: 'ソシレ', 
     symbol: 'G', 
-    colorName: 'ピンク', 
+    colorName: 'ぴんく', 
     color: '#EC4899', 
     notes: ['G4', 'B4', 'D5'], 
     abc: '[GBd]' 
@@ -69,7 +69,7 @@ export const ChordDefinitions = {
     id: 'sodomi', 
     name: 'ソドミ', 
     symbol: 'C/G', 
-    colorName: '茶', 
+    colorName: 'ちゃいろ', 
     color: '#A3744D', 
     notes: ['G3', 'C4', 'E4'], 
     abc: '[G,CE]' 
@@ -80,7 +80,7 @@ export const ChordDefinitions = {
     id: 'radosharpmi', 
     name: 'ラド#ミ', 
     symbol: 'A', 
-    colorName: '黄緑', 
+    colorName: 'きみどり', 
     color: '#84CC16', 
     notes: ['A3', 'C#4', 'E4'], 
     abc: '[A,^CE]' 
@@ -89,7 +89,7 @@ export const ChordDefinitions = {
     id: 'refasharpara', 
     name: 'レファ#ラ', 
     symbol: 'D', 
-    colorName: '肌色', 
+    colorName: 'はだいろ', 
     color: '#F4A460', 
     notes: ['D4', 'F#4', 'A4'], 
     abc: '[D^FA]' 
@@ -98,7 +98,7 @@ export const ChordDefinitions = {
     id: 'misosharpshi', 
     name: 'ミソ#シ', 
     symbol: 'E', 
-    colorName: '薄紫', 
+    colorName: 'うすむらさき', 
     color: '#DDA0DD', 
     notes: ['E4', 'G#4', 'B4'], 
     abc: '[E^GB]' 
@@ -107,7 +107,7 @@ export const ChordDefinitions = {
     id: 'shiflatrefa', 
     name: 'シ♭レファ', 
     symbol: 'B♭', 
-    colorName: '灰色', 
+    colorName: 'はいいろ', 
     color: '#6B7280', 
     notes: ['Bb3', 'D4', 'F4'], 
     abc: '[_B,DF]' 
@@ -116,7 +116,7 @@ export const ChordDefinitions = {
     id: 'miflatshiblat', 
     name: 'ミ♭ソシ♭', 
     symbol: 'E♭', 
-    colorName: '水色', 
+    colorName: 'みずいろ', 
     color: '#06B6D4', 
     notes: ['Eb4', 'G4', 'Bb4'], 
     abc: '[_EG_B]' 
@@ -125,7 +125,7 @@ export const ChordDefinitions = {
 
 export const Levels = [
   {
-    name: 'Level 1: 基本の3和音（赤・黄・青）',
+    name: 'Level 1: 基本の3和音（あか・きいろ・あお）',
     description: 'まずはここから。絶対音感トレーニングの基本となる3つの和音。',
     chords: [
       ChordDefinitions.DOMISO,
